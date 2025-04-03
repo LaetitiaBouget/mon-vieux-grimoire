12e741b016a1f54f06be84d0d54ad8d582b8d230
Laetitia Bouget <laetitia.bouget@yahoo.com>
2025-04-03 22:57:20 +0200

    middleware authentification

--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const auth = require('auth');
+
 const router = express.Router();
+
 
 const booksCtrl = require('../controllers/books');
 
 router.get('/', booksCtrl.getAllBooks);
 router.get('/:id', booksCtrl.getOneBook);
-router.post('/', booksCtrl.createBook);
-router.put('/:id', booksCtrl.modifyBook);
-router.delete('/:id', booksCtrl.deleteBook);
+router.post('/', auth, booksCtrl.createBook);
+router.put('/:id', auth, booksCtrl.modifyBook);
+router.delete('/:id', auth, booksCtrl.deleteBook);
 
 module.exports = router;
