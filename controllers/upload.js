d358fb9eed9a421192d34a6cdd94cd62d771d526
Laetitia Bouget <laetitia.bouget@yahoo.com>
2025-04-24 21:23:08 +0200

    correction upload image

--- controllers/upload.js
+++ controllers/upload.js
@@ -5,10 +5,15 @@ const sharp = require('sharp');
 const imagesDir = (path.join(__dirname,'../images'));
 
 exports.uploadImage = async (req, res,  next) => {
-
+const isCreating = req.method ==='POST';
 
     //If the user don't modify the image, then go to the next middleware
-    if (!req.file) return next();
+    if (!req.file) {
+        if (isCreating) {
+            return res.status(400).json({ message: "Une image est requise." })
+        }
+        return next();
+    }
     
     try {
         fs.access(imagesDir, (error) => {
